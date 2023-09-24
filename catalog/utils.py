ddf0f0935997e873d06a6753b5401d1de344fabb
Pavel1991-lab <sotnikov.pavel.91@mail.ru>
2023-09-24 14:29:17 +0300

    mail121

--- catalog/utils.py
+++ catalog/utils.py
@@ -5,23 +5,28 @@ from catalog.classDB_meneger_ import DBManager
 from django.core.mail import send_mail
 from datetime import datetime
 
-from catalog.models import Product
+from catalog.models import Product, Client
 from config import settings
 
 
 def my_scheduled_job():
-    current_time = datetime.now().strftime('%H:%M')
+    current_time = datetime.now().time()
+    current_date = datetime.now().date()
     all_product = Product.objects.all()
-    print(all_product)
+    all_client = Client.objects.all()
     for product in all_product:
-        send_mail(
-            subject=product.topic,
-            message='hi',
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=["sotnikov.pavel.91@mail.ru"]
-        )
-
-
+        print(current_time)
+        print(current_date)
+        print(all_product)
+        print(all_client)
+        for client in all_client:
+            if product.mailing_time <= current_time and product.mailing_date == current_date:
+                send_mail(
+                    subject=product.topic,
+                    message=product.description,
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[client.email]
+                )
 
 
 #
